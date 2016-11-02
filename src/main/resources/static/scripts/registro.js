2917e9cb95eadd8171fa13883b904624700b1a09
Johan Sebastian Peña Peñaloza <johan.pena-p@mail.escuelaing.edu.co>
2016-11-01 21:15:57 -0500

    fix al registro

--- src/main/resources/static/scripts/registro.js
+++ src/main/resources/static/scripts/registro.js
@@ -1,54 +1,41 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-function registro () {
+function registro() {
     var usuario = $("#user").val();
     var email = $("#email").val();
     var contra = $("#pass1").val();
     var contraConfirmacion = $("#pass2").val();
-    var nombre = $("#nombre").val().concat($("#apellido").val());
+    var nombre = $("#nombre").val().concat(" " + $("#apellido").val());
     
-    if(usuario !== null && email !== null && contra !== null && contraConfirmacion !== null && nombre !== null){
+    if(usuario !== "" && email !== "" && contra !== "" && contraConfirmacion !== "" && nombre !== ""){
         if(contra === contraConfirmacion) { 
-                chainedPromises = function(){
-            putRequest();
-          };
-          
-          putRequest = function() {
-              return $.ajax({
-                  url: '/register/user',
-                  type: 'POST',
-                  data: JSON.stringify({
-                      name: nombre,
-                      pwd: contra,
-                      uname: usuario,
-                      mail: email
-                  }),
-                  contentType: "application/json",
-                  success: function(data) {
-                      window.location = "http://localhost:8080/pages/login.html";
-                      alert("Bienvenido: " + data.name);
-                  },
-                  error: function(xhr, status){
-                      alert("Usted no se ha autenticado correctamente.");
-                  }
-               });
-          };        
-
-          chainedPromises();    
+            putRequest(nombre, contra, usuario, email);
         }
         else{
             alert("Las contraseñas no coinciden.");
         }
-        
     }
-    
     else{
         alert("Porfavor verifique que ha ingresado todos los campos del formulario.");
     }
    
 }
+
+putRequest = function(nombre, contra, usuario, email) {
+    return $.ajax({
+        url: '/register/user',
+        type: 'POST',
+        data: JSON.stringify({
+            name: nombre,
+            pwd: contra,
+            uname: usuario,
+            mail: email
+        }),
+        contentType: "application/json",
+        success: function(data) {
+            window.location = "http://localhost:8080/pages/login.html";
+            alert(data);
+        },
+        error: function(xhr, status){
+            alert("No se ha podido registrar su información, por favor, verifiquela.");
+        }
+    });
+};
